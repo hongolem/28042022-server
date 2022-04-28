cb60511a1ca68f45c33ef574153ec4c972bae185
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-04-28 11:35:16 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 radio.set_group(100)
+A = 0
+B = 0
+C = 0
+D = 0
+E = 0
 
-def on_received_value(answer, value):
-    radio.send_string("")
+def on_received_value(name, value):
+    global A, B, C, D, E
+    if value == 65:
+        A += 1
+    elif value == 66:
+        B += 1
+    elif value == 67:
+        C += 1
+    elif value == 68:
+        D += 1
+    elif value == 69:
+        E += 1
 radio.on_received_value(on_received_value)
-control.device_serial_number()
--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
 radio.setGroup(100)
-radio.onReceivedValue(function on_received_value(answer: string, value: number) {
-    radio.sendString("")
+let A = 0
+let B = 0
+let C = 0
+let D = 0
+let E = 0
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+    if (value == 65) {
+        A += 1
+    } else if (value == 66) {
+        B += 1
+    } else if (value == 67) {
+        C += 1
+    } else if (value == 68) {
+        D += 1
+    } else if (value == 69) {
+        E += 1
+    }
+    
 })
-control.deviceSerialNumber()
