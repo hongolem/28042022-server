05898bfec4338e8081f93a2cac37b0b0f6d3bcc9
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-05-08 13:49:21 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#Při zmáčknutí tlačítka A na serveru se odešle číslo 1, 
+# při kterém se u klienta povolí hlasování (možnosti od A do E), zobrazí "Start!", 
+# pošle se do serveru hodnota a z ní se vyčte sériové číslo, které se uloží do listu sériových čísel, 
+# poté se server i klient přehodí na jinou rádiovou skupinu (101) a server klientovi pošle zprávu o příjmutí 
+# a oba microbity se znovu přehodí na původní rádio skupinu (100).
+#Do té doby, pokud zmáčkne tlačítko nebo pin, tak se zobrazí "Hlasování nezačalo!".
+#Hlasování jde vynulovat pomocí zmáčknutí loga, ale hlasování se neukončí.
+#Zmáčknutím tlačítka B na serveru se odešle číslo 0, které na klientovi zobrazí "End!" a uzamkne hlasování 
+# a určí hodnotu odpovědi (64-69 od "", "A" až po "E") na základě jaká je proměnná Decision 
+# a danná odpověď se odešle pod proměnnou 'Answer' s jménem value "answer".
+#V serveru se přičtou body podle příjmuté hodnoty (64-69) k jednotlivým proměnným, 
+# které se po půl sekundové pauze (kvůli jistotě započtení všech hlasů), printnou do konzole.
+
 radio.set_group(100)
 
 Serial_numbers = [0,]
@@ -14,6 +27,7 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     radio.send_number(0)
+    basic.pause(500)
     print("Pro možnost A hlasovalo " + A + " lidí.")
     print("Pro možnost B hlasovalo " + B + " lidí.")
     print("Pro možnost C hlasovalo " + C + " lidí.")
--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+// Při zmáčknutí tlačítka A na serveru se odešle číslo 1, 
+//  při kterém se u klienta povolí hlasování (možnosti od A do E), zobrazí "Start!", 
+//  pošle se do serveru hodnota a z ní se vyčte sériové číslo, které se uloží do listu sériových čísel, 
+//  poté se server i klient přehodí na jinou rádiovou skupinu (101) a server klientovi pošle zprávu o příjmutí 
+//  a oba microbity se znovu přehodí na původní rádio skupinu (100).
+// Do té doby, pokud zmáčkne tlačítko nebo pin, tak se zobrazí "Hlasování nezačalo!".
+// Hlasování jde vynulovat pomocí zmáčknutí loga, ale hlasování se neukončí.
+// Zmáčknutím tlačítka B na serveru se odešle číslo 0, které na klientovi zobrazí "End!" a uzamkne hlasování 
+//  a určí hodnotu odpovědi (64-69 od "", "A" až po "E") na základě jaká je proměnná Decision 
+//  a danná odpověď se odešle pod proměnnou 'Answer' s jménem value "answer".
+// V serveru se přičtou body podle příjmuté hodnoty (64-69) k jednotlivým proměnným, 
+//  které se po půl sekundové pauze (kvůli jistotě započtení všech hlasů), printnou do konzole.
 radio.setGroup(100)
 let Serial_numbers = [0]
 let Bez_odpovědi = 0
@@ -11,6 +23,7 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     radio.sendNumber(0)
+    basic.pause(500)
     console.log("Pro možnost A hlasovalo " + A + " lidí.")
     console.log("Pro možnost B hlasovalo " + B + " lidí.")
     console.log("Pro možnost C hlasovalo " + C + " lidí.")
