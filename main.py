ff3fa318de011918b18fc333cbdfdd43a5e58e05
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-05-08 12:36:28 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 radio.set_group(100)
 
-Serial_numbers = []
+Serial_numbers = [0,]
 Bez_odpovědi = 0
 A = 0
 B = 0
@@ -14,16 +14,32 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     radio.send_number(0)
+    print("Pro možnost A hlasovalo " + A + " lidí.")
+    print("Pro možnost B hlasovalo " + B + " lidí.")
+    print("Pro možnost C hlasovalo " + C + " lidí.")
+    print("Pro možnost D hlasovalo " + D + " lidí.")
+    print("Pro možnost E hlasovalo " + E + " lidí.")
+    print("Nehlasovalo " + Bez_odpovědi + " lidí.")
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def on_received_string(receivedString):
+def on_logo_event_pressed():
+    Bez_odpovědi = 0
+    A = 0
+    B = 0
+    C = 0
+    D = 0
+    E = 0
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+def on_received_value2(serial_number, value):
     global Serial_numbers
-    Serial_numbers.append(receivedString)
+    Serial_numbers.append(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))
     radio.set_group(101)
     radio.send_string("Napojeno se serverem!")
-radio.on_received_string(on_received_string)
+    radio.set_group(100)
+radio.on_received_value(on_received_value2)
 
-def on_received_value(name, value):
+def on_received_value(answer, value):
     global Bez_odpovědi, A, B, C, D, E
     if value == 64:
         Bez_odpovědi += 1
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 radio.setGroup(100)
-let Serial_numbers = []
+let Serial_numbers = [0]
 let Bez_odpovědi = 0
 let A = 0
 let B = 0
@@ -11,14 +11,29 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     radio.sendNumber(0)
+    console.log("Pro možnost A hlasovalo " + A + " lidí.")
+    console.log("Pro možnost B hlasovalo " + B + " lidí.")
+    console.log("Pro možnost C hlasovalo " + C + " lidí.")
+    console.log("Pro možnost D hlasovalo " + D + " lidí.")
+    console.log("Pro možnost E hlasovalo " + E + " lidí.")
+    console.log("Nehlasovalo " + Bez_odpovědi + " lidí.")
 })
-radio.onReceivedString(function on_received_string(receivedString: string) {
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    let Bez_odpovědi = 0
+    let A = 0
+    let B = 0
+    let C = 0
+    let D = 0
+    let E = 0
+})
+radio.onReceivedValue(function on_received_value2(serial_number: string, value: number) {
     
-    Serial_numbers.push(receivedString)
+    Serial_numbers.push(radio.receivedPacket(RadioPacketProperty.SerialNumber))
     radio.setGroup(101)
     radio.sendString("Napojeno se serverem!")
+    radio.setGroup(100)
 })
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
+radio.onReceivedValue(function on_received_value(answer: string, value: number) {
     
     if (value == 64) {
         Bez_odpovědi += 1
